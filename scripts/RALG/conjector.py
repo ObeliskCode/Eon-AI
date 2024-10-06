720053fcbc263e52eb58e37851e338f67fbf0bcf
Noel <n6garcia@ucsd.edu>
2024-10-06 02:07:23 -0700

    new functions

--- scripts/RALG/conjector.py
+++ scripts/RALG/conjector.py
@@ -4,6 +4,40 @@ def infn_set(x):
     """Generate a set of informal numbers based on x."""
     return set([-x - 1, -x, -x + 1, 0, x - 1, x, x + 1])
 
+def infn_mult(x, y):
+    """Add two informal sets x and y."""
+    set_x = infn_set(x)
+    set_y = infn_set(y)
+
+    result = set()
+    for num_x in set_x:
+        for num_y in set_y:
+            result.mult(num_x * num_y)
+
+    return sorted(result)
+
+def infn_div(x, y):
+    """Append informal number y to informal set x."""
+    set_x = x
+    set_y = infn_set(y)
+
+    result = set()
+    for num_x in set_x:
+        for num_y in set_y:
+            result.add(num_x * num_y)
+
+    return sorted(result)
+
+def infn_mult_extend(informal_set_x, informal_set_y):
+    """Append informal number y to informal set."""
+    result = set()
+
+    for num_x in informal_set_x:
+        for num_y in informal_set_y:
+            result.add(num_x * num_y)
+
+    return sorted(result)
+
 def infn_add(x, y):
     """Add two informal sets x and y."""
     set_x = infn_set(x)
@@ -29,7 +63,7 @@ def infn_append(x, y):
     return sorted(result)
 
 
-def infn_extend(informal_set_x, informal_set_y):
+def infn_add_extend(informal_set_x, informal_set_y):
     """Append informal number y to informal set."""
     result = set()
 
@@ -53,7 +87,7 @@ def H(informal_combination):
         infn = informal_nums[0]
 
     for infn_x in informal_nums[1:]:
-        infn = infn_extend(infn, infn_x)  # Use infn_append to build the set
+        infn = infn_add_extend(infn, infn_x)  # Use infn_append to build the set
 
     total = sum(infn)  # Calculate total of the combined informal set
 
