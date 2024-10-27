4f1baecce25a6cb3b61e98afba465f60d446e8f0
Noel <n6garcia@ucsd.edu>
2024-10-26 23:17:25 -0700

    comment

--- scripts/INF/infmath.py
+++ scripts/INF/infmath.py
@@ -1,9 +1,7 @@
 def infn_set(x):
-    """Generate a set of informal numbers based on x."""
     return set([-x - 1, -x, -x + 1, 0, x - 1, x, x + 1])
 
 def infn_mult(x, y):
-    """Add two informal sets x and y."""
     set_x = infn_set(x)
     set_y = infn_set(y)
 
@@ -15,7 +13,6 @@ def infn_mult(x, y):
     return sorted(result)
 
 def infn_mult_append(x, y):
-    """Append informal number y to informal set x."""
     set_x = x
     set_y = infn_set(y)
 
@@ -27,7 +24,6 @@ def infn_mult_append(x, y):
     return sorted(result)
 
 def infn_mult_extend(informal_set_x, informal_set_y):
-    """Append informal number y to informal set."""
     result = set()
 
     for num_x in informal_set_x:
@@ -37,7 +33,6 @@ def infn_mult_extend(informal_set_x, informal_set_y):
     return sorted(result)
 
 def infn_add(x, y):
-    """Add two informal sets x and y."""
     set_x = infn_set(x)
     set_y = infn_set(y)
 
@@ -49,7 +44,6 @@ def infn_add(x, y):
     return sorted(result)
 
 def infn_append(x, y):
-    """Append informal number y to informal set x."""
     set_x = x
     set_y = infn_set(y)
 
@@ -62,7 +56,6 @@ def infn_append(x, y):
 
 
 def infn_add_extend(informal_set_x, informal_set_y):
-    """Append informal number y to informal set."""
     result = set()
 
     for num_x in informal_set_x:
