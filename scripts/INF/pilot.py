9e93da73f5fe2c41539cae7db96304df7e6ac594
Noel <n6garcia@ucsd.edu>
2024-12-14 21:22:39 -0800

    pilot.py

--- scripts/INF/pilot.py
+++ scripts/INF/pilot.py
@@ -1,33 +1,24 @@
-## tensorflow objective:
 
-## the first principle of this AI is the stack & recursion
+## Reinforcement Learning for ILC AI
 
-## so imagine black-jack or 21 then imagine that the whole
-## number provided is when you're number goes bust based
-## on it's maximum either leading to and ilc or an ilc
+## 5 features
 
-## that means that the model has to have an input
-## shape of 1 and output shape of 1 recursively.
+## 1. the target number
+## 2. amount of guesses already made
+## 3. current maximum (total)
+## 4. incomplete/complete
+## 5. amount of missing numbers in set
 
-## the point of this is that the model will
-## be made in two points as an empty tuple
-## by default and an only a single addition
-## to the tuple and it's target black-jack number
+## Output is then the next guess in the ILC
 
-## so the model needs to be geared to be able
-## to train with mutable variables of size n and a number then
-## output to a single number
+## So an ILC is built one addition at a time
+## as the learning enviroment or (game)
+## that the reinforcement learning will try
+## to complete optimally
 
-## I need to retool this pilot.py to make this possible
-## how do I train an AI which has a mutable input size
-## to begin with? [TODO]
-
-## the black-jack prediction model should be a model
-## which feeds into another prediction machine
-## the problem is that it should be a purely statistical
-## machine which means MSE should be perfect
-## for a hyper-parameter tuning machine
-## which will grade the machine for potential
-## loss at every point, card game AI.
+## a simple test could be a simple NN designed
+## around 5 input neurons to 1 output neuron
 
 ## https://www.tensorflow.org/guide/basics
+
+## https://www.tensorflow.org/agents/tutorials/0_intro_rl
